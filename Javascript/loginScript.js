ce7075e90f10375fc2611c8ec580044c221a78f2
Shikhar Khandelwal <shikhark@Shikhars-MacBook-Pro.local>
2018-11-04 12:32:15 +0800

    LoginHTML done

---
+++ Javascript/loginScript.js
@@ -0,0 +1,41 @@
+var usernameField=document.getElementById("username");
+var passwordField=document.getElementById("password");
+var submitBtn = document.getElementById("submit");
+var username='',password='';
+usernameField.onblur=()=>{username=usernameField.value}
+passwordField.onblur=()=>{password=passwordField.value}
+
+
+
+submitBtn.onclick=sendLoginRequest
+
+
+
+
+
+
+
+function sendLoginRequest(){
+    console.log(username+"   "+password)
+    if(username==''|| password==''){
+        alert("Please do not leave fields empty.")
+    }else{
+        var xmlhttp;
+        if (window.XMLHttpRequest) {
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                console.log("Achieved")
+            }
+        }
+        xmlhttp.open("POST", "../PHP/verifyLogin.php",true);
+        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xmlhttp.send("username="+username+"&password="+password);   
+    }
+    
+
+
+}
