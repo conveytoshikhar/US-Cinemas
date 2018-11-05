77be17921ca7878072dc9ee041e0a9fe6d83846f
Shikhar Khandelwal <shikhark@Shikhars-MacBook-Pro.local>
2018-11-06 02:08:15 +0800

    Done some CSS

--- PHP/main.php
+++ PHP/main.php
@@ -3,6 +3,7 @@
   <head>
   <meta charset="utf-8"/>
     <title>US Cinemas</title>
+    <link rel="stylesheet" type="text/css" href="../CSS/mainStyling.css">
   </head>
 
   <body>
@@ -11,12 +12,15 @@
             session_start();
             
             if(isAuthenticated()){
+
+                echo "<header>";
                 echo "<ul>";
                 echo "<li><a href='buywelcome.php'>Buy a Ticket</a></li>";
                 echo "<li><a href='comment.php'>Movie Review</a></li>";
                 echo "<li><a href='history.php'>Purchase History</a></li>";
                 echo "<li><a href='logout.php'>Logout</a></li>";
                 echo "<ul>";
+                echo "</header>";
             }else{
                 echo "<h1> You have not logged in. </h1>";
                 echo "<script> setTimeout(  ()=>{window.location.replace('../index.html')}  , 3000) </script>";
