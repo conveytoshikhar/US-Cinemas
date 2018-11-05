86d5060513bd52e6d15a43f9cb501ab8a230bb65
Shikhar Khandelwal <shikhark@Shikhars-MacBook-Pro.local>
2018-11-05 11:27:37 +0800

    Table almost done

--- PHP/seatplantry.php
+++ PHP/seatplantry.php
@@ -25,7 +25,13 @@
                 $house=$result[3];
                 $category=$result[4];
                 $day=$result[5];
-
+                
+                $_SESSION['date']=$date;
+                $_SESSION['time']=$time;
+                $_SESSION['filmName']=$filmName;
+                $_SESSION['house']=$house;
+                $_SESSION['category']=$category;
+                $_SESSION['day']=$day;
                 echo "<h1>Ticketing</h1>";
 
                 echo "<h3> Cinema:      US</h3>";
@@ -40,8 +46,43 @@
 
 
                 //create table here 
+                $db_conn=mysqli_connect('sophia.cs.hku.hk','andelwal','Shikhar1','andelwal') 
+                or die("Error is "+mysqli_connect_error());
 
-    
+                $houseid=getHouseNumber($house);
+                $query="SELECT * FROM house WHERE id='$houseid'";  
+                $house_record=mysqli_query($db_conn,$query) or die("Query Error! ".mysqli_error($db_conn) ); 
+                $rows=0;
+                $columns=0;
+                if(mysqli_num_rows($house_record)>0){
+                    while($house=mysqli_fetch_array($house_record)){
+                        $rows=$house['houserow'];
+                        $columns=$house['housecol'];
+                    }
+                }
+
+                echo "<form action='buyticket.php' method='POST'>";
+                echo '<table id="seatingPlan" style="border: 1px solid black">';
+                for($r=1;$r<=$rows;$r++){
+                    echo '<tr>';
+                    for($c=1;$c<=$columns;$c++){
+                        echo '<td>';
+                        echo '<div">';
+                        $rowAlphabet=getRowsAlphabet($r);
+                        echo "<input type='checkbox' name='seat' value='$rowAlphabet&$c'>";
+                        echo "<p> $rowAlphabet$c </p>";
+                        echo '</div>';
+                        echo '</td>';
+                    }
+                    echo '</tr>';
+                }
+                echo '</table>';
+                echo '<input type="submit" value="Submit">';
+                echo '</form>';
+                echo '<button id="cancel">Cancel</button>';
+
+
+
 
 
 
@@ -82,8 +123,19 @@
                 return $number;
             }
 
+            function getRowsAlphabet($rowid){
+                $row='A';
+                for($i=1;$i<$rowid;$i++){
+                    $row++;
+                }
+                return $row;
+            }
+        
         ?>
-
+        <script>
+            var cancel=document.getElementById("cancel");
+            cancel.onclick=()=>{window.location.replace('buywelcome.php')};
+        </script>
     </body>
 
 </html>
